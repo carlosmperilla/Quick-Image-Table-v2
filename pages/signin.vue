d182660cedafaab2886a08fd841ac46913223afa
Carlos Perilla <carlosperillaprogramacion@gmail.com>
2023-05-12 07:23:07 -0300

    .

--- pages/signin.vue
+++ pages/signin.vue
@@ -49,10 +49,12 @@
 
         signinErrors.length = 0
         const { username, password } = userSignData
-        await signIn('credentials', { username, password, redirect: false})
+        // await signIn('credentials', { username, password, redirect: false})
+        const { error:eje, url:aja } = await signIn('credentials', { username, password, redirect: false})
         const { error, url } = await signIn('credentials', { username, password, redirect: false})
         
-        console.log(url)
+        console.log('1', eje, aja)
+        console.log('2', error, url)
         if (error) {
             const messages = JSON.parse(error).messages
             for (let message of messages) {
