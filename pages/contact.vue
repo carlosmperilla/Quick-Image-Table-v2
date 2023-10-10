872bec1f89cae00a9b91ef81d862bc374d9db0bd
Carlos Perilla <105413945+carlosmperilla@users.noreply.github.com>
2023-10-09 22:44:49 -0300

    Update contact.vue
    probando hootsuite

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,7 +4,7 @@
             <h1>Contacto</h1>
         </header>
         <section>
-            <h2>Puedes ver mis proyectos en desarrollo aquí 1,2,3:</h2>
+            <h2>Puedes ver mis proyectos en desarrollo aquí:</h2>
             <p>
                 <a href="https://github.com/carlosmperilla" target="_blank" rel="noopener">https://github.com/carlosmperilla</a>
             </p>
@@ -20,6 +20,7 @@
             <p class="page-email" tabindex="0" title="e-mail de contacto" alt="e-mail de contacto">
                 carlosperillaprogramacion@gmail.com
             </p>
+            <a href="https://hootsuite.com/auth/authorize?client_id=lo0rtFoLazDDlsXWXElE&amp;nonce=f1185ef621b14b214548579407d514bf501144a8f977164eaf8d66f26c663768&amp;redirect_uri=https%3A%2F%2Fplatform.hootsuite.com%2Foauth2%2Fconsent&amp;response_type=id_token&amp;state=Y2xpZW50X3N0YXRlPXJlc3BvbnNlX3R5cGUlM0Rjb2RlJTI2Y2xpZW50X2lkJTNENGZkNDYwYTAtN2U4Ny00ZGM2LWE4NDAtOTkzZTViNmE0NGU0JTI2cmVkaXJlY3RfdXJpJTNEaHR0cHMlM0ElMkYlMkZ3d3cuZ2V0cG9zdG1hbi5jb20lMkZvYXV0aDIlMkZjYWxsYmFjayUyNnNjb3BlJTNEb2ZmbGluZSZzaWduYXR1cmU9cCVCNSUyOCVGOFlVdiUwMiVDOSVERSVBNCUyOFclMEMlMDUlRUUlRUElOEUlOEIlMDZlJURGJUQxdCUwNyVFNmhCJUQzTyVBNCVDNSVFOCUwNSVBNSU5OSU4NiUxMTQlQzYlQjUlQ0ZRJTlEJUFFJUYyJTAyMiVBNTYlRTglRjhkWSU5QmklREIlRkFYJUM5cTYlNjAlQTA%3D">Found</a>.
         </section>
     </article>
 </template>
